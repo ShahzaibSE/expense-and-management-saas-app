146498570fe3cefbf7c3a06cbe7868c824576e9b
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2024-09-24 22:33:31 +0500

    Title: Rate Limiting
    Desc: I have added rate limiting to prevent DDoS attacks and preventing the origination of any possible bottlenecks in order to make the server more fault tolerant.

--- backend/api-gateway-service/kafka-producer.py
+++ backend/api-gateway-service/kafka-producer.py
@@ -1,6 +1,11 @@
 from kafka import KafkaProducer
+import json
 
-kafka_producer = KafkaProducer()
+kafka_producer = KafkaProducer(
+    bootstrap_server=['broker:9092'],
+    value_serializer=lambda x: json.dump(x).encode('utf-8')
+)
 
 def send_log(log_data:str):
-    kafka_producer.send(log_data)
+    kafka_producer.send('log_data',log_data)
+    kafka_producer.flush()
--- backend/api-gateway-service/main.py
+++ backend/api-gateway-service/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from slowapi.middleware import SlowAPIASGIMiddleware
+from slowapi.errors import RateLimitExceeded
+from slowapi import Limiter, _rate_limit_exceeded_handler
+import socket
 
+
+limiter: Limiter = Limiter(key_func=lambda request: request.client.host)
+
+# print(f"Client Host: {socket.gethostname()}")
 
 app: FastAPI = FastAPI()
+
+app.middleware(SlowAPIASGIMiddleware)
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 @app.get("/")
 def home():
