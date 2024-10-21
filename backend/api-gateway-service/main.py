1efa0e671f2b5322383d30a6173a5e8fac90c3a0
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2024-10-21 19:36:43 +0500

    feat: Added security headers using secure.py

--- backend/api-gateway-service/main.py
+++ backend/api-gateway-service/main.py
@@ -5,16 +5,38 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from routes.user_service_proxy import setup_routes
 from environment_manager import EnvironmentManager
 from fastapi.middleware.cors import CORSMiddleware
+from secure import Secure
 
 # print(f"Client Host: {socket.gethostname()}")
 limiter: Limiter = Limiter(key_func=lambda request: request.client.host)
 
 app: FastAPI = FastAPI()
 
+# Initialize Secure instance
+secure = Secure(
+    content_security_policy="default-src 'self'",
+    hsts=True,
+    hsts_include_subdomains=True,
+    hsts_preload=True,
+    referrer="no-referrer",
+    x_content_type_options=True,
+    x_dns_prefetch_control=False,
+    x_frame_options="DENY",
+    x_xss_protection=1
+)
+
+
 # Add SlowAPI middleware for rate limiting
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIASGIMiddleware)
+
+@app.middleware("http")
+async def set_secure_headers(request, call_next):
+    response = await call_next(request)
+    # Add security headers using the secure package
+    secure.framework.fastapi(response)
+    return response
 
 # Configure CORS
 app.add_middleware(
