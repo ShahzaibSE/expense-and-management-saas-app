1e8c05b20edad24fefe3955f03aed56cf4630cca
ShahzaibSE <shahzaibnoorcs@gmail.com>
2024-11-21 00:28:59 +0500

    WIP: Creating the landing page.

--- frontend/src/custom_components/welcome.tsx
+++ frontend/src/custom_components/welcome.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,12 +6,37 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
 
 export default function WelcomeComponent() {
   return (
-    <div>WelcomeComponent</div>
-  )
+    <Card className="w-full">
+      <CardHeader className="flex flex-col justify-center items-center">
+        <CardTitle><span className="text-xl">Expense Management</span></CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col justify-between items-center">
+        <Button className="w-full my-2 p-2">
+          Create Account
+        </Button>
+        <Separator className="my-6 border-gray-400" />
+        <div className="w-full">
+          <Button className="w-full my-1">
+            Sign In with Email
+          </Button>
+          <Button className="w-full">
+            Sign In with Google
+          </Button>
+        </div>
+      </CardContent>
+      <CardFooter className="flex flex-col justify-between items-center text-sm text-gray-500">
+        <div>
+          <h1 className="font-bold">Terms & Services</h1>
+        </div>
+      </CardFooter>
+    </Card>
+  );
 }
